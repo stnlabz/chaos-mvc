ebb0e51df4eb01a31810d23f5e8c0879401966ac
Poe Mei <poemei761@gmail.com>
2026-03-16 13:04:38 -0700

    Update nav.php

--- app/views/inc/nav.php
+++ app/views/inc/nav.php
@@ -2,6 +2,7 @@
   <div class="nav-container">
     <ul>
       <li><a href="/">Home</a></li>
+      <li><a href="/changelog">Changelog</a></li>
       <li><a href="/developer">Developer</a></li>
       <li><a href="/downloads">Download</a></li>
       <li><a href="/github">Quick Start</a></li>
