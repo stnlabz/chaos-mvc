49613befd658b50d0150be459a9da91b782fb383
Poe Mei <poemei761@gmail.com>
2026-04-06 07:25:04 -0700

    Update home.php

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -6,17 +6,17 @@ final class home extends controller
     public function index($url_params = null): void
     {
         // 1. Load the models/plugins
-        $announcements = $this->model('announcements_model'); 
+        //$announcements = $this->model('announcements_model'); 
 
         // 2. Fetch data from both sources
-        $latest = $announcements->get_latest_single();
+        //$latest = $announcements->get_latest_single();
 
         // 3. Package everything into ONE data array
-        $data = [
+        /**$data = [
             'featured_announcement' => $latest
         ];
-
+        */
         // 4. Pass the combined $data to the view
-        $this->view('public/home/index', $data);
+        $this->view('public/home/index');
     }
 }
